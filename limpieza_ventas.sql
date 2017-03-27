1965b721e051e7175d6e3b103a740947ebac96ec
Alejandro Flores Martinez <alexfire94@outlook.com>
2017-03-27 13:02:34 -0600

    Bdd Actualizada

--- limpieza_ventas.sql
+++ limpieza_ventas.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-03-2017 a las 18:38:37
--- Versión del servidor: 5.7.14
--- Versión de PHP: 5.6.25
+-- Tiempo de generación: 27-03-2017 a las 19:00:47
+-- Versión del servidor: 5.7.11
+-- Versión de PHP: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -44,18 +44,19 @@ CREATE TABLE `productos` (
   `iva` decimal(9,0) UNSIGNED DEFAULT NULL,
   `mu_pv` varchar(10) DEFAULT NULL,
   `mu_pm` varchar(10) DEFAULT NULL,
-  `unidad_medida` varchar(30) DEFAULT NULL
+  `unidad_medida` varchar(30) DEFAULT NULL,
+  `cant_mayoreo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`idProductos`, `clave_prod`, `nombre_prod`, `descrip_prod`, `estatus_prod`, `cant_prod`, `stock_max`, `stock_min`, `marca_prod`, `prov_idProv`, `precio_compra`, `precio_venta`, `precio_mayoreo`, `img`, `iva`, `mu_pv`, `mu_pm`, `unidad_medida`) VALUES
-(12, 123234134, 'Escoba Madera', 'Madera', 'Disponible', '16', '50', '5', 'Escobar', 'Escobedo', '15', '19', '17', NULL, '16', '150', NULL, 'Pieza'),
-(13, 12345, 'JABON LIQUIDO', 'JABON PARA LAVAR TRASTES', 'Disponible', '4', '50', '5', 'AXIÓN', 'ABARROTES SAN PEDRO', '5', '10', '8', NULL, '16', '5', NULL, 'Litro'),
-(14, 132, 'JABON LIQUIDO', 'DE ROPA', 'Disponible', '15', '50', '5', 'MAS COLOR', 'COLORES', '5', '9', '7', NULL, '16', '4', NULL, 'Litro'),
-(15, 123, 'JABON LIQUIDO', 'LAVA TRASTES', 'Disponible', '50', '50', '5', 'AXION', 'JABONES COLORES', '5', '10', '8', NULL, '16', '5', NULL, 'Litro');
+INSERT INTO `productos` (`idProductos`, `clave_prod`, `nombre_prod`, `descrip_prod`, `estatus_prod`, `cant_prod`, `stock_max`, `stock_min`, `marca_prod`, `prov_idProv`, `precio_compra`, `precio_venta`, `precio_mayoreo`, `img`, `iva`, `mu_pv`, `mu_pm`, `unidad_medida`, `cant_mayoreo`) VALUES
+(12, 123234134, 'Escoba Madera', 'Madera', 'Disponible', '16', '50', '5', 'Escobar', 'Escobedo', '15', '19', '17', NULL, '16', '150', NULL, 'Pieza', 0),
+(13, 12345, 'JABON LIQUIDO', 'JABON PARA LAVAR TRASTES', 'Disponible', '4', '50', '5', 'AXIÓN', 'ABARROTES SAN PEDRO', '5', '10', '8', NULL, '16', '5', NULL, 'Litro', 0),
+(14, 132, 'JABON LIQUIDO', 'DE ROPA', 'Disponible', '15', '50', '5', 'MAS COLOR', 'COLORES', '5', '9', '7', NULL, '16', '4', NULL, 'Litro', 0),
+(15, 123, 'JABON LIQUIDO', 'LAVA TRASTES', 'Disponible', '50', '50', '5', 'AXION', 'JABONES COLORES', '5', '10', '8', NULL, '16', '5', NULL, 'Litro', 0);
 
 --
 -- Índices para tablas volcadas
